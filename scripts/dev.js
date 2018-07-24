8109338b3374b91207cbfe49f660e8430b16c4ea
AWolf81 <awolf2904@gmail.com>
2018-07-24 08:43:52 +0200

    WIP: Tried IIFE & config - still not working.

--- scripts/dev.js
+++ scripts/dev.js
@@ -8,21 +8,41 @@ const file = Path.join(__dirname, "./src/index.html");
 console.log("file", file);
 
 const options = {
-  //outDir: './dist', // The out directory to put the build files in, defaults to dist
-  //outFile: 'index.html', // The name of the outputFile
-  publicUrl: "./" // The url to server on, defaults to dist
+  outDir: "./dist", // The out directory to put the build files in, defaults to dist
+  outFile: "index.html", // The name of the outputFile
+  publicUrl: "./dist", // The url to server on, defaults to dist
+  watch: false, // whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  cache: true, // Enabled or disables caching, defaults to true
+  cacheDir: ".cache", // The directory cache gets put in, defaults to .cache
+  minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
+  target: "browser", // browser/node/electron, defaults to browser
+  https: false, // Server files over https or http, defaults to false
+  logLevel: 3, // 3 = log everything, 2 = log warnings & errors, 1 = log errors
+  hmrPort: 0, // The port the hmr socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
+  sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (not supported in minified builds yet)
+  hmrHostname: "", // A hostname for hot module reload, default to ''
+  detailedReport: false // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
 };
 
-async function runBundle() {
+function runBundle() {
   // Initializes a bundler using the entrypoint location and options provided
   const bundler = new Bundler(file, options);
 
-  bundler.on("bundled", bundle => {
-    // bundler contains all assets and bundles, see documentation for details
-    // gets called once after first bundle
-  });
+  // bundler.on("buildEnd", () => {
+  //   // run post-build scripts
+  //   spawn("yarn", ["start:electron"], {
+  //     stdio: "inherit",
+  //     shell: true
+  //   });
+  // });
 
-  bundler.bundle();
+  // bundler.on("bundled", bundle => {
+  //   // bundler contains all assets and bundles, see documentation for details
+  //   // gets called once after first bundle
+  //   //spawn("yarn", ["start:electron"], { stdio: "inherit", shell: true });
+  // });
+
+  //bundler.bundle();
 
   // Run the bundler, this returns the main bundle
   // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
@@ -33,7 +53,10 @@ async function runBundle() {
       shell: true
     });
   });
-  await bundle;
+
+  return bundle;
 }
 
-runBundle();
+(async function() {
+  await runBundle();
+})();
